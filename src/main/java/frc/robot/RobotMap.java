8bcaca1e263f2d31accae02ed0a545f197999ea4
Ian Dai <56898254+IanWearsHat@users.noreply.github.com>
2021-03-10 22:55:43 -0800

    Created RobotMap

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+public class RobotMap {
+
+    public static class DriveTrainMap {
+        //Constants for DriveTrain
+    }
+
+    public static class ElevatorMap {
+        //Constants for Elevator
+    }
+
+    public static class ArmMap {
+        //Constants for Arm
+    }
+
+}
