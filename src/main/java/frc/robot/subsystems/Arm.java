4cc8a2a8bcb84b8e49b73f6a535108a57a67a4b4
Rayna-Carranza <1061410@etusd.org>
2021-04-10 18:07:44 -0700

    Rayna is almost done finally

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,14 +1,20 @@
+package frc.robot.subsystems;
+
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 
 
+import javax.tools.ForwardingFileObject;
+
 import com.ctre.phoenix.motorcontrol.can.MotControllerJNI;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.XboxController;
+
 
 /** Add your docs here. */
 public class Arm extends SubsystemBase {
@@ -17,10 +23,17 @@ public class Arm extends SubsystemBase {
   public WPI_TalonFX rightFollower;
   public WPI_TalonFX rightLeader;
   public WPI_TalonFX leftLeader;
+  private WPI_TalonFX wpi_TalonFX;
 
-  @Override
-  public void intiDefaultCommand
 
-  public Arm(WPI_TalonFX leftFollower) {
-    this.leftFollower = leftFollower;
+
+  public void MoveForwardArm(WPI_TalonFX leftFollower) {
+  extracted(leftFollower);
   } 
+
+  private void extracted(WPI_TalonFX leftFollower2) {
+  }
+
+  public void RotateArm(WPI_TalonFX rightFollowerFx) {
+    extracted(rightFollowerFx);
+  };
