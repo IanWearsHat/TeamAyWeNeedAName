3c30c274f58cd95899f23c3f09c017abc138d88c
Shoaib <1027114@etusd.org>
2021-04-10 18:11:28 -0700

    Finished Elevator Down :O

--- src/main/java/frc/robot/commands/ElevatorDown.java
+++ src/main/java/frc/robot/commands/ElevatorDown.java
@@ -30,7 +30,7 @@ public class ElevatorDown extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    elevator.setposition(0); 
+    elevator.setposition(1000); 
   }
 
   // Called every time the scheduler runs while the command is scheduled.
