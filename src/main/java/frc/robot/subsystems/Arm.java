733a35a734621f3ca6a0531ab488f95bbeed783d
Rayna-Carranza <1061410@etusd.org>
2021-04-10 19:20:10 -0700

    The arm subsytem

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -6,34 +6,39 @@ package frc.robot.subsystems;
 
 
 
-import javax.tools.ForwardingFileObject;
 
-import com.ctre.phoenix.motorcontrol.can.MotControllerJNI;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.XboxController;
-
+import edu.wpi.first.wpilibj.XboxController.Button;
 
 /** Add your docs here. */
 public class Arm extends SubsystemBase {
-  private static final Command Initializing = null;
-  public WPI_TalonFX leftFollower;
-  public WPI_TalonFX rightFollower;
-  public WPI_TalonFX rightLeader;
-  public WPI_TalonFX leftLeader;
-  private WPI_TalonFX wpi_TalonFX;
+  public WPI_TalonFX leftFx;
+  public WPI_TalonFX rightFx;
+  public WPI_TalonFX middleFx;
+  
 
 
+  public void MoveForwardArm(WPI_TalonFX leftFx) {
+    extracted(leftFx);
+  };
 
-  public void MoveForwardArm(WPI_TalonFX leftFollower) {
-  extracted(leftFollower);
-  } 
+  private void extracted(WPI_TalonFX leftFx) {
+  
+  };
 
-  private void extracted(WPI_TalonFX leftFollower2) {
+  public void RotateArm(WPI_TalonFX rightFx) {
+    extracted(rightFx);
+  };
+
+  public void StopArm(WPI_TalonFX middleFx) {
+
   }
 
-  public void RotateArm(WPI_TalonFX rightFollowerFx) {
-    extracted(rightFollowerFx);
-  };
+  public  void Open(Arm subsystemArm) {}
+  void XboxController (Button Y)
+  {};
+  public void close(Arm subsyteArm){}
+  void XboxController2 (Button X)
+  {}};
+
