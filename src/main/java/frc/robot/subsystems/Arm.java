b08f92ab23a2a5f358c6f92a552408332c011858
Ian Dai <56898254+IanWearsHat@users.noreply.github.com>
2021-04-11 18:08:30 -0700

    removed xbox controller from arm

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -4,38 +4,26 @@ package frc.robot.subsystems;
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-
-
-
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.XboxController.Button;
 
 /** Add your docs here. */
 public class Arm extends SubsystemBase {
-  public WPI_TalonFX leftFx;
-  public WPI_TalonFX rightFx;
-  public WPI_TalonFX middleFx;
+    public WPI_TalonFX leftFx;
+    public WPI_TalonFX rightFx;
+    public WPI_TalonFX middleFx;
 
-  public Arm() {
-    leftFx = new WPI_TalonFX(10);
-    rightFx = new WPI_TalonFX(11);
-    middleFx = new WPI_TalonFX(12);
-  }
+    public Arm() {
+      leftFx = new WPI_TalonFX(10);
+      rightFx = new WPI_TalonFX(11);
+      middleFx = new WPI_TalonFX(12);
+    }
 
-  public void moveForwardArm(WPI_TalonFX LeftFx) {
+    public void moveForwardArm(WPI_TalonFX LeftFx) {
 
-  }
+    }
 
-  public void rotateArm(WPI_TalonFX RightFx) {
+    public void rotateArm(WPI_TalonFX RightFx) {
 
-  }
-
-  public  void Open(Arm subsystemArm) {}
-  void XboxController (Button Y)
-  {};
-  public void close(Arm subsyteArm){}
-  void XboxController2 (Button X)
-  {}
-
+    }
   }
